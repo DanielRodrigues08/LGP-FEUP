ff85bf01b06f33f7cf84810bfadef01d0a3e0f49
Luka Nestic <luka.nestic555@gmail.com>
2024-04-06 13:39:56 +0100

    Project cleanup

---
+++ frontend/src/components/HomeItem.vue
@@ -0,0 +1,13 @@
+<script setup>
+import SampleItem from './SampleItem.vue'
+import SampleIcon from './icons/SampleIcon.vue'
+</script>
+
+<template>
+  <SampleItem>
+    <template #icon>
+      <SampleIcon />
+    </template>
+    <template #heading>Sample Text</template>
+  </SampleItem>
+</template>
