28974f5984924818435eca093aed31c629fa77cf
Henrique-O-S <henri11silvafcc@gmail.com>
2024-04-13 20:03:45 +0100

    User setup

---
+++ backend/src/main/java/com/lifecycle/backend/model/User.java
@@ -0,0 +1,36 @@
+package com.lifecycle.backend.model;
+
+import lombok.*;
+
+import jakarta.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private UserRole role;
+
+    public User(String username, String password, UserRole role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
+
