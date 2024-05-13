6b159a2a16ea4c2b57f24229bb8c4a90f18d9077
Daniel Rodrigues <up202006562@up.pt>
2024-05-13 10:40:00 +0100

    when an onboardee changes its process, then all step info associated to that onboardee will be deleted

--- backend/src/main/java/com/lifecycle/backend/repository/StepInfoRepository.java
+++ backend/src/main/java/com/lifecycle/backend/repository/StepInfoRepository.java
@@ -6,8 +6,10 @@ import com.lifecycle.backend.model.Step;
 import com.lifecycle.backend.model.StepInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StepInfoRepository extends JpaRepository<StepInfo, Long> {
     Optional<StepInfo> findByOnboardeeAndStep(Onboardee onboardee, Step step);
+    List<StepInfo> findByOnboardee(Onboardee onboardee);
 }
