e43fa83a65247f0a34a63fdfd5de0b02e048aa35
Daniel Rodrigues <up202006562@up.pt>
2024-05-23 12:03:10 +0100

    download csv

--- frontend/src/views/OnboardeesView.vue
+++ frontend/src/views/OnboardeesView.vue
@@ -3,27 +3,23 @@
     <div class="search-container">
       <div>
         <InputText v-model="globalFilter.value" placeholder="Search for name" @input="filterOnboardees" />
-      </div>  
+      </div>
       <router-link to="/add-onboardee">
-          <Button class="add-button" label="New onboardee" icon="pi pi-plus" />
+        <Button class="add-button" label="New onboardee" icon="pi pi-plus" />
       </router-link>
+      <Button @click="downloadCSV" class="add-button" label="CSV" icon="pi pi-download"/>
     </div>
-    
-    <div v-for="(stateOnboardees, state) in filteredOnboardees" :key="state" >
+
+    <div v-for="(stateOnboardees, state) in filteredOnboardees" :key="state">
       <h2 style="margin-left:5%; margin-top: 20px;" :class="stateClass(state)">{{ toTitleCase(state) }} onboardees</h2>
-      <DataTable
-        class="custom-table"
-        :value="stateOnboardees"
-        :globalFilter="globalFilter"
-        @row-click="handleRowClick"
-        tableStyle="min-width: 50rem"
-        removableSort
-        paginator :rows="5" :rowsPerPageOptions="[5,10,20,50]"
-      >
-        <Column field="name" sortable header="Name" :headerStyle="{ backgroundColor: '#033A65', color:'white'}"></Column>
-        <Column field="email" header="Email" :headerStyle="{ backgroundColor: '#033A65', color:'white'}"></Column>
-        <Column field="phoneNumber" header="Phone" :headerStyle="{ backgroundColor: '#033A65', color:'white'}"></Column>
-        <Column field="startDate" header="Start date" :headerStyle="{ backgroundColor: '#033A65', color:'white'}"></Column>
+      <DataTable class="custom-table" :value="stateOnboardees" :globalFilter="globalFilter" @row-click="handleRowClick"
+        tableStyle="min-width: 50rem" removableSort paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]">
+        <Column field="name" sortable header="Name" :headerStyle="{ backgroundColor: '#033A65', color: 'white' }">
+        </Column>
+        <Column field="email" header="Email" :headerStyle="{ backgroundColor: '#033A65', color: 'white' }"></Column>
+        <Column field="phoneNumber" header="Phone" :headerStyle="{ backgroundColor: '#033A65', color: 'white' }"></Column>
+        <Column field="startDate" header="Start date" :headerStyle="{ backgroundColor: '#033A65', color: 'white' }">
+        </Column>
       </DataTable>
     </div>
   </div>
@@ -31,7 +27,7 @@
 
 <script>
 import DataTable from 'primevue/datatable';
-import Column from 'primevue/column'; 
+import Column from 'primevue/column';
 import axios from 'axios';
 import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
@@ -74,7 +70,7 @@ export default {
   methods: {
     async fetchOnboardees() {
       try {
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}/onboardees`, {headers: this.authStore.authData()});
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/onboardees`, { headers: this.authStore.authData() });
         this.onboardees = response.data;
         this.originalOnboardees = response.data;
       } catch (error) {
@@ -91,7 +87,7 @@ export default {
         this.onboardees = this.originalOnboardees;
       } else {
         this.onboardees = this.originalOnboardees.filter(item => item.name.toLowerCase().includes(this.globalFilter.value.toLowerCase()));
-      }    
+      }
     },
     handleRowClick(event) {
       const onboardeeId = event.data.id;
@@ -107,6 +103,21 @@ export default {
     },
     toTitleCase(str) {
       return str.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    },
+    downloadCSV(){
+      console.log(this.onboardees)
+      let csv = 'name,email,phoneNumber,startDate,state\n';
+      for(const row of this.onboardees){
+        csv += `${row['name']},${row['email']},${row['phoneNumber']},${row['startDate']},${row['state']}\n`
+      }
+      console.log(csv)
+      const blob = new Blob([csv], { type: 'text/csv' });
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'onboardees.csv';
+      a.click();
+      window.URL.revokeObjectURL(url);
     }
   }
 };
@@ -118,32 +129,40 @@ h3 {
   margin-left: 2.1rem;
   margin-top: 1.5rem;
 }
+
 .incoming {
   color: green;
 }
+
 .ongoing {
   color: #1976D2;
 }
+
 .completed {
   color: purple;
 }
+
 .aborted {
   color: #C81E1E;
 }
+
 .add-button {
   background-color: #033A65;
   color: white;
-  border-color: rgba(0, 0, 0, 0.1); 
+  border-color: rgba(0, 0, 0, 0.1);
 }
+
 .search-container {
   display: flex;
   align-items: center;
   margin-top: 1.5rem;
   margin-left: 2rem;
 }
-.search-container > div {
+
+.search-container>div {
   margin-right: 1rem;
 }
+
 .custom-table {
   max-width: 90%;
   margin: 0 auto;
